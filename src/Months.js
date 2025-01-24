abae7fd6ffb70d8c89fff61791a17c53a6723c16
unknown <operator@stacja42-01.szkola.zst-radom.edu.pl>
2025-01-24 11:26:09 +0100

    New component - Months

---
+++ src/Months.js
@@ -0,0 +1,47 @@
+function Months() {
+    return (
+        <>
+        <h1>Miesiące</h1>
+        <ol>
+            <li>
+                Styczeń
+            </li>
+            <li>
+                Luty
+            </li>
+            <li>
+                Marzec
+            </li>
+            <li>
+                Kwiecień
+            </li>
+            <li>
+                Maj
+            </li>
+            <li>
+                Czerwiec
+            </li>
+            <li>
+                Lipiec
+            </li>
+            <li>
+                Sierpień
+            </li>
+            <li>
+                Wrzesień
+            </li>
+            <li>
+                Październik
+            </li>
+            <li>
+                Listopad
+            </li>
+            <li>
+                Grudzień
+            </li>
+        </ol>
+        </>
+    );
+}
+
+export default Months;
