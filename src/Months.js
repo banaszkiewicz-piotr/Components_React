4da434e0788389eb3bcdfda258850f9b2dc8696d
unknown <operator@stacja42-01.szkola.zst-radom.edu.pl>
2025-02-26 14:15:32 +0100

    Modified Months.js

--- src/Months.js
+++ src/Months.js
@@ -5,39 +5,51 @@ function Months() {
         <ol>
             <li>
                 Styczeń
+                <p>31</p>
             </li>
             <li>
                 Luty
+                <p>28/29</p>
             </li>
             <li>
                 Marzec
+                <p>31</p>
             </li>
             <li>
                 Kwiecień
+                <p>30</p>
             </li>
             <li>
                 Maj
+                <p>31</p>
             </li>
             <li>
                 Czerwiec
+                <p>30</p>
             </li>
             <li>
                 Lipiec
+                <p>31</p>
             </li>
             <li>
                 Sierpień
+                <p>31</p>
             </li>
             <li>
                 Wrzesień
+                <p>30</p>
             </li>
             <li>
                 Październik
+                <p>31</p>
             </li>
             <li>
                 Listopad
+                <p>30</p>
             </li>
             <li>
                 Grudzień
+                <p>31</p>
             </li>
         </ol>
         </>
